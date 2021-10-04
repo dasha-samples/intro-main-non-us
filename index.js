5f2253b7cfc4595bcfc947d9f33d4cb347091a7c
anzhelikami <87754588+anzhelikami@users.noreply.github.com>
2021-10-04 15:29:03 +0300

    update index.js to new sdk

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ async function main() {
 
   app.ttsDispatcher = () => "Default";
 
-  app.connectionProvider = async (conv) =>
-    conv.input.phone === "chat"
-      ? dasha.chat.connect(await dasha.chat.createConsoleChat())
-      : dasha.sip.connect(new dasha.sip.Endpoint("default"));
-  await app.start();
+
 
   const conv = app.createConversation({
     phone: process.argv[2],
   });
 
-  if (conv.input.phone !== "chat") conv.on("transcription", console.log);
+  await app.start();
+
+  conv.audio.tts = "dasha";
+
+  if (conv.input.phone === "chat") {
+    await dasha.chat.createConsoleChat(conv);
+  } else {
+    conv.on("transcription", console.log);
+  }
 
   const logFile = await fs.promises.open("./log.txt", "w");
   await logFile.appendFile("#".repeat(100) + "\n");
@@ -32,7 +36,9 @@ async function main() {
     }
   });
 
-  const result = await conv.execute();
+  const result = await conv.execute({
+    channel: conv.input.phone === "chat" ? "text" : "audio",
+  });
   console.log(result.output);
   await app.stop();
   app.dispose();
